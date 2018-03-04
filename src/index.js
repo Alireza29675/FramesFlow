2310ff455887c504a47a87d48d653f760a7c4aae
Alireza29675 <alireza.sheikholmolouki@gmail.com>
2018-03-04 19:49:45 +0330

    restarting again 🌊

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import FramesFlow from './components/FramesFlow'
-
-// defining framesFlow as a global object
-if (!(window.framesFlow instanceof FramesFlow)) { // if it didn't define before
-    window.framesFlow = new FramesFlow()
-}
-
-// export
-export default window.framesFlow
